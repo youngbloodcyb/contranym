ed2fbbefc2068a52c83a4ce227fbedc020dd1667
Cameron Youngblood <cameron.youngblood@gmail.com>
2026-01-27 22:21:42 -0800

    add db package

---
+++ packages/db/src/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+
+export const db = drizzle({
+    connection: {
+        connectionString: process.env.DATABASE_URL!,
+        ssl: true
+    }
+});
---
+++ packages/db/src/types.ts
@@ -0,0 +1,45 @@
+export type {
+    User,
+    NewUser,
+    Account,
+    NewAccount,
+    Contact,
+    NewContact,
+    Deal,
+    NewDeal,
+    Activity,
+    NewActivity,
+    Task,
+    NewTask,
+    Note,
+    NewNote,
+    Tag,
+    NewTag,
+} from "./schema";
+
+// Enum types
+export type {
+    accountStageEnum,
+    dealStageEnum,
+    contactRoleEnum,
+    activityTypeEnum,
+    taskStatusEnum,
+    taskPriorityEnum,
+} from "./schema";
+
+// Inferred enum value types
+import type {
+    accountStageEnum,
+    dealStageEnum,
+    contactRoleEnum,
+    activityTypeEnum,
+    taskStatusEnum,
+    taskPriorityEnum,
+} from "./schema";
+
+export type AccountStage = (typeof accountStageEnum.enumValues)[number];
+export type DealStage = (typeof dealStageEnum.enumValues)[number];
+export type ContactRole = (typeof contactRoleEnum.enumValues)[number];
+export type ActivityType = (typeof activityTypeEnum.enumValues)[number];
+export type TaskStatus = (typeof taskStatusEnum.enumValues)[number];
+export type TaskPriority = (typeof taskPriorityEnum.enumValues)[number];
